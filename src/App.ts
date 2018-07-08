517d50a79a372b5ee1f33fb1803f5002e0331fbc
Maciej Barelkowski <maciej@moodup.team>
2018-07-08 21:30:40 +0200

    Add mongo config

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import { config } from './config';
 import { router as telemetriesRouter } from './Telemetries';
+
+mongoose.connect(process.env.DB_URL);
+mongoose.Promise = global.Promise;
+const dbConnection = mongoose.connection;
+
+dbConnection.on('error', (error: any) => console.error('MongoDB connection error.', error));
+dbConnection.once('open', () => console.log('Connected to MongoDB database: ' + dbConnection.db.databaseName));
 
 const app = express();
 
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
+const mongoDbUrl = process.env.MONGODB_URL;
 const port = process.env.PORT || 3000;
 
 export const config = {
+  mongoDbUrl,
   port,
 };
